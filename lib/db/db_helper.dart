08874b52232e89d94f02a898854d85979eacb564
Ramil <ramilka06@inbox.ru>
2021-03-15 14:37:11 +0300

    add notify

--- lib/db/db_helper.dart
+++ lib/db/db_helper.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 import 'dart:async';
 
 import 'package:authApp/models/profileModel.dart';
-import 'package:flutter/material.dart';
+import 'package:authApp/providers/auth_provider.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
@@ -12,6 +12,7 @@ class DbHelper {
   DbHelper._();
   static final DbHelper db = DbHelper._();
   static Database _database;
+  AuthProvider provider = AuthProvider();
 
   Future<Database> get database async {
     if (_database != null) {
@@ -24,11 +25,11 @@ class DbHelper {
   initDB() async {
     Directory documentDirectory = await getExternalStorageDirectory();
     print(documentDirectory.path);
-    String path = join(documentDirectory.path, 'authApp.db');
+    String path = join(documentDirectory.path, 'authApp.sql');
     return await openDatabase(path, version: 1, onOpen: (db) {},
         onCreate: (Database db, int version) async {
       await db.execute("CREATE TABLE profile("
-          "id INTEGER PRIMARY KEY,"
+          "id TEXT,"
           "last_name TEXT,"
           "first_name TEXT,"
           "patronymic TEXT,"
@@ -38,100 +39,105 @@ class DbHelper {
           "category TEXT,"
           "transfer_status INTEGER"
           ")");
-      await db.execute("CREATE TABLE user_data("
-          "key TEXT NOT NULL UNIQUE,"
-          "value TEXT"
-          ")");
+      // await db.execute("CREATE TABLE user_data("
+      //     "key TEXT NOT NULL UNIQUE,"
+      //     "value TEXT"
+      //     ")");
     });
   }
 
-  insertUserData(ProfileModel profile) async {
-    final db = await database;
-    await db.insert("user_data", profile.toMapLastName());
-    await db.insert("user_data", profile.toMapFirstName());
-    await db.insert("user_data", profile.toMapPatronymic());
-    await db.insert("user_data", profile.toMapEmail());
-    await db.insert("user_data", profile.toMapPhone());
-    await db.insert("user_data", profile.toMapSex());
-    await db.insert("user_data", profile.toMapCategory());
-  }
-
-  Future<ProfileModel> readProfileData() async {
-    final db = await database;
-    var lastName =
-        await db.query("user_data", where: "key=?", whereArgs: ["lastName"]);
-    var firstName =
-        await db.query("user_data", where: "key=?", whereArgs: ["firstName"]);
-    var patronymic =
-        await db.query("user_data", where: "key=?", whereArgs: ["patronymic"]);
-    var email =
-        await db.query("user_data", where: "key=?", whereArgs: ["email"]);
-    var phone =
-        await db.query("user_data", where: "key=?", whereArgs: ["phone"]);
-    var sex = await db.query("user_data", where: "key=?", whereArgs: ["sex"]);
-    var category =
-        await db.query("user_data", where: "key=?", whereArgs: ["category"]);
-    return lastName.isNotEmpty && firstName.isNotEmpty
-        ? ProfileModel.fromMapUserData(
-            lastName.first,
-            firstName.first,
-            patronymic.first,
-            email.first,
-            phone.first,
-            sex.first,
-            category.first)
-        : null; //@TODO
-  }
-
-  Future<ProfileModel> updateProfileData(ProfileModel profile) async {
-    final db = await database;
-    await db.update("user_data", profile.toMapLastName(),
-        where: "key=?", whereArgs: ["lastName"]);
-    var lastName =
-        await db.query("user_data", where: "key=?", whereArgs: ["lastName"]);
-    print(lastName.toString());
-    await db.update("user_data", profile.toMapLastName(),
-        where: "key=?", whereArgs: ["firstName"]);
-    await db.update("user_data", profile.toMapLastName(),
-        where: "key=?", whereArgs: ["patronymic"]);
-    await db.update("user_data", profile.toMapLastName(),
-        where: "key=?", whereArgs: ["email"]);
-    await db.update("user_data", profile.toMapLastName(),
-        where: "key=?", whereArgs: ["phone"]);
-    await db.update("user_data", profile.toMapLastName(),
-        where: "key=?", whereArgs: ["sex"]);
-    await db.update("user_data", profile.toMapLastName(),
-        where: "key=?", whereArgs: ["category"]);
-  }
-
-  fetch() async {
-    final db = await database;
-    final result = await db.query("user_data");
-
-    return result
-        .map((e) => ProfileModel.fromMapUserData(e['lastName'], e['firstName'],
-            e['patronymic'], e['email'], e['phone'], e['sex'], e['category']))
-        .toList();
-  }
-
-  // insertProfile(ProfileModel profile) async {
+  // insertUserData(ProfileModel profile) async {
   //   final db = await database;
-  //   var result = db.insert("profile", profile.toMap());
-  //   return result;
+  //   // db.insert("profile", values);
+  //   // await db.insert("user_data", profile.toMapLastName());
+  //   // await db.insert("user_data", profile.toMapFirstName());
+  //   // await db.insert("user_data", profile.toMapPatronymic());
+  //   // await db.insert("user_data", profile.toMapEmail());
+  //   // await db.insert("user_data", profile.toMapPhone());
+  //   // await db.insert("user_data", profile.toMapSex());
+  //   // await db.insert("user_data", profile.toMapCategory());
   // }
 
-  // Future<ProfileModel> readProfile(int id) async {
+  // Future<ProfileModel> readProfileData() async {
   //   final db = await database;
-  //   var result = await db.query("profile", where: "id=?", whereArgs: [id]);
-  //   return result.isNotEmpty ? ProfileModel.fromMap(result.first) : null;
+  //   var lastName =
+  //       await db.query("user_data", where: "key=?", whereArgs: ["lastName"]);
+  //   var firstName =
+  //       await db.query("user_data", where: "key=?", whereArgs: ["firstName"]);
+  //   var patronymic =
+  //       await db.query("user_data", where: "key=?", whereArgs: ["patronymic"]);
+  //   var email =
+  //       await db.query("user_data", where: "key=?", whereArgs: ["email"]);
+  //   var phone =
+  //       await db.query("user_data", where: "key=?", whereArgs: ["phone"]);
+  //   var sex = await db.query("user_data", where: "key=?", whereArgs: ["sex"]);
+  //   var category =
+  //       await db.query("user_data", where: "key=?", whereArgs: ["category"]);
+  //   return lastName.isNotEmpty && firstName.isNotEmpty
+  //       ? ProfileModel.fromMapUserData(
+  //           lastName.first,
+  //           firstName.first,
+  //           patronymic.first,
+  //           email.first,
+  //           phone.first,
+  //           sex.first,
+  //           category.first)
+  //       : null; //@TODO
   // }
 
-  // updateProfile(ProfileModel profile) async {
+  // Future<ProfileModel> updateProfileData(ProfileModel profile) async {
   //   final db = await database;
-  //   var result = await db.update("profile", profile.toMap(),
-  //       where: "id = ?", whereArgs: [profile.id]);
-  //   return result;
+  //   await db.update("user_data", profile.toMapLastName(),
+  //       where: "key=?", whereArgs: ["lastName"]);
+  //   var lastName =
+  //       await db.query("user_data", where: "key=?", whereArgs: ["lastName"]);
+  //   print(lastName.toString());
+  //   await db.update("user_data", profile.toMapLastName(),
+  //       where: "key=?", whereArgs: ["firstName"]);
+  //   await db.update("user_data", profile.toMapLastName(),
+  //       where: "key=?", whereArgs: ["patronymic"]);
+  //   await db.update("user_data", profile.toMapLastName(),
+  //       where: "key=?", whereArgs: ["email"]);
+  //   await db.update("user_data", profile.toMapLastName(),
+  //       where: "key=?", whereArgs: ["phone"]);
+  //   await db.update("user_data", profile.toMapLastName(),
+  //       where: "key=?", whereArgs: ["sex"]);
+  //   await db.update("user_data", profile.toMapLastName(),
+  //       where: "key=?", whereArgs: ["category"]);
   // }
+
+  // fetch() async {
+  //   final db = await database;
+  //   final result = await db.query("user_data");
+
+  //   return result
+  //       .map((e) => ProfileModel.fromMapUserData(e['lastName'], e['firstName'],
+  //           e['patronymic'], e['email'], e['phone'], e['sex'], e['category']))
+  //       .toList();
+  // }
+
+  insertProfile(ProfileModel profile) async {
+    final db = await database;
+    var result = db.insert("profile", profile.toMap());
+    return result;
+  }
+
+  Future<ProfileModel> readProfile() async {
+    final db = await database;
+    var result = await db.query("profile");
+    return result.isNotEmpty ? ProfileModel.fromMap(result.first) : null;
+  }
+
+  updateProfile(ProfileModel profile) async {
+    final db = await database;
+    var result = await db.update("profile", profile.toMap());
+    return result;
+  }
+
+  deleteProfile() async {
+    final db = await database;
+    db.delete("profile");
+  }
 
   // static Future<Database> database() async {
   //   final dbPath = await sql.getDatabasesPath();
