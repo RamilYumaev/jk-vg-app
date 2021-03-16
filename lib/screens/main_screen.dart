0a2c1b9b0ae010353ad535e28bfaf85269b5aac1
Ramil <ramilka06@inbox.ru>
2021-03-16 19:50:38 +0300

    add screen

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -3,26 +3,184 @@ import 'package:authApp/drawers/main_drawer.dart';
 import 'package:authApp/providers/auth_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import '../models/push_notification.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:overlay_support/overlay_support.dart';
 
-class MainScreen extends StatelessWidget {
+PushNotification _notificationInfo;
+int _totalNotifications;
+
+class MainScreen extends StatefulWidget {
   static const routeName = '/';
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  FirebaseMessaging _messaging = FirebaseMessaging();
+
+  void registerNotification() async {
+    // Initialize the Firebase app
+    await Firebase.initializeApp();
+
+    // On iOS, this helps to take the user permissions
+    await _messaging.requestNotificationPermissions(
+      IosNotificationSettings(
+        alert: true,
+        badge: true,
+        provisional: false,
+        sound: true,
+      ),
+    );
+
+    // For handling the received notifications
+    _messaging.configure(
+      onMessage: (message) async {
+        print('onMessage received: $message');
+
+        PushNotification notification = PushNotification.fromJson(message);
+
+        setState(() {
+          _notificationInfo = notification;
+          _totalNotifications++;
+        });
+
+        // For displaying the notification as an overlay
+        showSimpleNotification(
+          Text(_notificationInfo.title),
+          leading: NotificationBadge(totalNotifications: _totalNotifications),
+          subtitle: Text(_notificationInfo.body),
+          background: Colors.cyan[700],
+          duration: Duration(seconds: 2),
+        );
+      },
+      onBackgroundMessage: _firebaseMessagingBackgroundHandler,
+      onLaunch: (message) async {
+        print('onLaunch: $message');
+
+        PushNotification notification = PushNotification.fromJson(message);
+
+        setState(() {
+          _notificationInfo = notification;
+          _totalNotifications++;
+        });
+      },
+      onResume: (message) async {
+        print('onResume: $message');
+
+        PushNotification notification = PushNotification.fromJson(message);
+
+        setState(() {
+          _notificationInfo = notification;
+          _totalNotifications++;
+        });
+      },
+    );
+
+    // Used to get the current FCM token
+    _messaging.getToken().then((token) {
+      print('Token: $token');
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  @override
+  void initState() {
+    _totalNotifications = 0;
+    registerNotification();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: MainDrawer(),
-        appBar: AppBar(
-          //  title: Text("Главная"),
-          actions: [
-            IconButton(
-                icon: Icon(Icons.exit_to_app),
-                onPressed: () {
-                  DbHelper.db.deleteProfile();
-                  Provider.of<AuthProvider>(context).logout();
-                })
-          ],
-          backgroundColor: Colors.blueGrey,
+      drawer: MainDrawer(),
+      appBar: AppBar(
+        //  title: Text("Главная"),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.exit_to_app),
+              onPressed: () {
+                DbHelper.db.deleteProfile();
+                Provider.of<AuthProvider>(context).logout();
+              })
+        ],
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'App for capturing Firebase Push Notifications',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(height: 16.0),
+          NotificationBadge(totalNotifications: _totalNotifications),
+          SizedBox(height: 16.0),
+          _notificationInfo != null
+              ? Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'TITLE: ${_notificationInfo.title ?? _notificationInfo.dataTitle}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      'BODY: ${_notificationInfo.body ?? _notificationInfo.dataBody}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ],
+                )
+              : Container(),
+        ],
+      ),
+    );
+  }
+}
+
+Future<dynamic> _firebaseMessagingBackgroundHandler(
+    Map<String, dynamic> message) async {
+  await Firebase.initializeApp();
+  print('onBackgroundMessage received: $message');
+}
+
+class NotificationBadge extends StatelessWidget {
+  final int totalNotifications;
+
+  const NotificationBadge({@required this.totalNotifications});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 40.0,
+      height: 40.0,
+      decoration: new BoxDecoration(
+        color: Colors.red,
+        shape: BoxShape.circle,
+      ),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            '$totalNotifications',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
         ),
-        body: Container());
+      ),
+    );
   }
 }
